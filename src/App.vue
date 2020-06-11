cb37b1dcc3491f38c6646c1d3868cca0ada36193
xionglinyong <1243010754@qq.com>
2020-06-11 14:53:48 +0800

    创建项目

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
-<template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<template lang="pug">
+  div( id="app" :class="$style.appInner")
+    header
+      router-link(to="/goods") goods
+      router-link(to="/personal") personal
+    div(v-if="loading" :class="$style.loading") loading....
+    div(:class="$style.appContainer" v-html="content") content
 </template>
 
-<style lang="stylus">
-#app
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  @Prop(Boolean) loading!: boolean
+  @Prop(String) content!: string
+}
+</script>
+
+<style lang="stylus" module>
+.appInner
   font-family Avenir, Helvetica, Arial, sans-serif
   -webkit-font-smoothing antialiased
   -moz-osx-font-smoothing grayscale
   text-align center
   color #2c3e50
-  margin-top 60px
+  margin-top 30px
+  display grid
+  grid-template-rows 100px auto
+  .loading
+    position absolute
+    width 100%
+    height 100%
+    background rgba(255,255,255,.5)
+    top 0%
+    left 0%
+    padding-top 200px
+    box-sizing border-box
+  .appContainer
+    width 100%
 </style>
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,66 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { registerMicroApps, start } from 'qiankun'
+
+interface RenderData {
+  appContent: string;
+  loading: boolean;
+}
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+let app: any = null
+
+function render ({ appContent, loading }: RenderData) {
+  if (!app) {
+    app = new Vue({
+      el: '#container',
+      router,
+      store,
+      data () {
+        return {
+          content: appContent,
+          loading
+        }
+      },
+      render (h) {
+        return h(App, {
+          props: {
+            content: appContent,
+            loading: loading
+          }
+        })
+      }
+    })
+  } else {
+    app.content = appContent
+    app.loading = loading
+  }
+}
+
+function initApp () {
+  render({ appContent: '', loading: false })
+}
+
+initApp()
+
+function genActioneRule (routerPrefix: any) {
+  return (location: Location) => location.pathname.startsWith(routerPrefix)
+}
+
+registerMicroApps([
+  {
+    name: 'goods-qiankun',
+    entry: 'http://localhost:80',
+    render,
+    activeRule: genActioneRule('/goods')
+  },
+  {
+    name: '@qiankun/personal',
+    entry: 'http://localhost:81',
+    render,
+    activeRule: genActioneRule('/personal')
+  }
+])
+start()
